e8699e6e362f72094a89edd2d1d20599312fd2b9
Bernardo Carvalho <bernardobcbeja@gmail.com>
2022-09-22 22:27:10 -0300

    Updating conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,6 @@
 # Bring your packages onto the path
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname('main'), '..')))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname('main'), '..')))
 import pytest
 from fastapi.testclient import TestClient
 from main import app
